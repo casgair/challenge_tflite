d1843e5bed004e65a4171430bd86a76d902aaa40
David <svantesson.david@gmail.com>
2022-07-19 17:04:22 +0200

    test.py changes for task 3

--- Silent-Face-Anti-Spoofing/test.py
+++ Silent-Face-Anti-Spoofing/test.py
@@ -7,10 +7,8 @@
 
 import os
 import cv2
-import numpy as np
 import argparse
 import warnings
-import time
 
 from src.anti_spoof_predict import AntiSpoofPredict
 from src.generate_patches import CropImage
@@ -31,7 +29,7 @@ def check_image(image):
         return True
 
 
-def test(image_name, model_dir, device_id):
+def test(image_name, model_dir, model_name, device_id):
     model_test = AntiSpoofPredict(device_id)
     image_cropper = CropImage()
     image = cv2.imread(SAMPLE_IMAGE_PATH + image_name)
@@ -39,52 +37,27 @@ def test(image_name, model_dir, device_id):
     if result is False:
         return
     image_bbox = model_test.get_bbox(image)
-    prediction = np.zeros((1, 3))
-    test_speed = 0
-    # sum the prediction from single model's result
-    for model_name in os.listdir(model_dir):
-        h_input, w_input, model_type, scale = parse_model_name(model_name)
-        param = {
-            "org_img": image,
-            "bbox": image_bbox,
-            "scale": scale,
-            "out_w": w_input,
-            "out_h": h_input,
-            "crop": True,
-        }
-        if scale is None:
-            param["crop"] = False
-        img = image_cropper.crop(**param)
-        start = time.time()
-        prediction += model_test.predict(img, os.path.join(model_dir, model_name))
-        test_speed += time.time()-start
+    h_input, w_input, model_type, scale = parse_model_name(model_name)
+    param = {
+        "org_img": image,
+        "bbox": image_bbox,
+        "scale": scale,
+        "out_w": w_input,
+        "out_h": h_input,
+        "crop": True,
+    }
+    if scale is None:
+        param["crop"] = False
 
-    # draw result of prediction
-    label = np.argmax(prediction)
-    value = prediction[0][label]/2
-    if label == 1:
-        print("Image '{}' is Real Face. Score: {:.2f}.".format(image_name, value))
-        result_text = "RealFace Score: {:.2f}".format(value)
-        color = (255, 0, 0)
-    else:
-        print("Image '{}' is Fake Face. Score: {:.2f}.".format(image_name, value))
-        result_text = "FakeFace Score: {:.2f}".format(value)
-        color = (0, 0, 255)
-    print("Prediction cost {:.2f} s".format(test_speed))
-    cv2.rectangle(
-        image,
-        (image_bbox[0], image_bbox[1]),
-        (image_bbox[0] + image_bbox[2], image_bbox[1] + image_bbox[3]),
-        color, 2)
-    cv2.putText(
-        image,
-        result_text,
-        (image_bbox[0], image_bbox[1] - 5),
-        cv2.FONT_HERSHEY_COMPLEX, 0.5*image.shape[0]/1024, color)
+    img = image_cropper.crop(**param)
+    prediction = model_test.predict(img, os.path.join(model_dir, model_name))
+    cv2.imwrite(SAMPLE_IMAGE_PATH + f"{image_name}_cropped.bmp", img)
 
-    format_ = os.path.splitext(image_name)[-1]
-    result_image_name = image_name.replace(format_, "_result" + format_)
-    cv2.imwrite(SAMPLE_IMAGE_PATH + result_image_name, image)
+    # Changed to not divide by 2, since only one model is used now
+    # Now only prints Genuine (Real face) score
+    value_real_face = prediction[0][1]
+
+    print("Image '{}' has Genuine (Real face) Score: {:.6f}.".format(image_name, value_real_face))
 
 
 if __name__ == "__main__":
@@ -101,9 +74,14 @@ if __name__ == "__main__":
         default="./resources/anti_spoof_models",
         help="model_lib used to test")
     parser.add_argument(
+        "--model_name",
+        type=str,
+        default="2.7_80x80_MiniFASNetV2.pth",
+        help="model file used to test")
+    parser.add_argument(
         "--image_name",
         type=str,
         default="image_F1.jpg",
         help="image used to test")
     args = parser.parse_args()
-    test(args.image_name, args.model_dir, args.device_id)
+    test(args.image_name, args.model_dir, args.model_name, args.device_id)
